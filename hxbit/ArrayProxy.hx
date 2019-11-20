c25d3846c7c546327b28dfd55cae87384ceef6f7
Nicolas Cannasse <ncannassse@gmail.com>
2019-11-20 11:39:21 +0100

    Reverts ec5b3e91a8ec007e6566ff4f4f4970857ba58c3e.

--- hxbit/ArrayProxy.hx
+++ hxbit/ArrayProxy.hx
@@ -70,10 +70,6 @@ abstract ArrayProxy<T>(ArrayProxyData<T>) {
 		return this.array.filter(t);
 	}
 
-	public function find( t : T->Bool ) {
-		return this.array.find(t);
-	}
-
 	public inline function indexOf(x:T, ?fromIndex) {
 		return this.array.indexOf(x, fromIndex);
 	}
@@ -207,10 +203,6 @@ abstract ArrayProxy2<T:ProxyChild>(ArrayProxyData<T>) {
 
 	public function filter( t : T->Bool ) {
 		return this.array.filter(t);
-	}
-
-	public function find( t : T->Bool ) {
-		return this.array.find(t);
 	}
 
 	public inline function indexOf(x:T, ?fromIndex) {
