c662bc2ae21fda00fa5197f213a99b90eceecdde
Nicolas Cannasse <ncannasse@gmail.com>
2022-04-14 16:19:50 +0200

    restore vars after set, more log with closure_debug

--- hxbit/Closure.hx
+++ hxbit/Closure.hx
@@ -50,10 +50,10 @@ abstract class Closure<T> #if !macro implements hxbit.Serializable #end {
 		}
 	}
 
-	static function replaceVars( ctx : { uid : Int, m : Map<Int,Bool> }, e : TypedExpr ) {
+	static function replaceVars( ctx : { uid : Int, m : Map<Int,{ v : TVar, prev : String }> }, e : TypedExpr ) {
 		inline function updateVar(v:TVar) {
 			if( !ctx.m.exists(v.id) ) {
-				ctx.m.set(v.id, true);
+				ctx.m.set(v.id, { v : v, prev : v.name });
 				var name = v.name == "`this" ? "__this" : "__"+ctx.uid++;
 				haxe.macro.TypeTools.setVarName(v,name);
 			}
@@ -80,21 +80,37 @@ abstract class Closure<T> #if !macro implements hxbit.Serializable #end {
 			prefix = prefix.charAt(0).toUpperCase() + prefix.substr(1) + "_" + Context.getLocalMethod() + "_L" + loc.range.start.line;
 			var sign = Context.signature(nullPos(f.expr)).substr(0, 6);
 			var ident = prefix + "_"+sign;
-			var constructVars : Array<String> = [];
+			var constructVars : Array<String> = null;
 			try {
 				Context.getType("hxbit.closure."+ident);
 			} catch( err ) {
 
+				constructVars = [];
+
+				#if closure_debug
+				Context.warning(funExpr.toString(), pos);
+				#end
+
 				var texpr = Context.withOptions({ allowInlining : false, allowTransform : false }, () -> Context.typeExpr(funExpr));
+
+				#if closure_debug
+				Context.warning(Context.getTypedExpr(texpr).toString(), pos);
+				#end
+
 				var t = switch( texpr.t ) {
 				case TFun(_,t): haxe.macro.TypeTools.toComplexType(t);
 				default: throw "assert";
 				}
 
 				var unbound = new Map();
+				var replaced = new Map();
 				gatherCapturedVars({ unbound : unbound, defined : new Map() }, texpr);
-				replaceVars({ uid : 0, m : new Map() },texpr);
+				replaceVars({ uid : 0, m : replaced },texpr);
 				var outExpr = Context.getTypedExpr(switch( texpr.expr ) { case TFunction(f): f.expr; default: throw "assert"; });
+
+				// restore previous names (cause bugs with `this)
+				for( v in replaced )
+					haxe.macro.TypeTools.setVarName(v.v, v.prev);
 
 				var fields : Array<Field> = [];
 				var vars = [];
@@ -152,7 +168,12 @@ abstract class Closure<T> #if !macro implements hxbit.Serializable #end {
 					meta : [{ name : ":access", pos : pos, params : [macro $p{currentClass.split(".")}] }],
 				});
 			}
-			return { expr : ENew({ pack : ["hxbit","closure"], name : ident },[for( v in constructVars ) macro $i{v}]), pos : pos };
+
+			var expr = { expr : ENew({ pack : ["hxbit","closure"], name : ident },[for( v in constructVars ) { expr : EConst(CIdent(v)), pos : pos }]), pos : pos };
+			#if closure_debug
+			Context.warning(expr.toString(), pos);
+			#end
+			return expr;
 		default:
 			Context.error("Expression should be a function with no argument", pos);
 			return macro null;
