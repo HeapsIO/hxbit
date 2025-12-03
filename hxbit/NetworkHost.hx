d00bf939840b8c621b916a4ac6a3c79b433d7d1a
ncannasse <ncannasse@gmail.com>
2025-12-03 22:36:33 +0100

    use length instead of pos (close #93)

--- hxbit/NetworkHost.hx
+++ hxbit/NetworkHost.hx
@@ -1090,7 +1090,7 @@ class NetworkHost {
 	function doSend() {
 		var bytes;
 		@:privateAccess {
-			if( ctx.out.pos == 0 ) return;
+			if( ctx.out.length == 0 ) return;
 			bytes = ctx.out.getBytes();
 			ctx.out = new haxe.io.BytesBuffer();
 		}
